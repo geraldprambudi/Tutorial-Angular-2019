c064ff8ee701566764151dc05d56feb7eb41da2c
geraldprambudi <geraldprambudi94@gmail.com>
2019-09-16 17:19:31 +0700

    #4 tampilkan data dan form edit data binding with bootstrap

--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Foods } from '../foods'; // tambahkan ini import foods.ts
+import { LAPERS } from '../makanan-indo'; // tambahkan ini dari makanan-indo.ts
+
 
 @Component({
   selector: 'app-food',
@@ -8,18 +10,16 @@ import { Foods } from '../foods'; // tambahkan ini import foods.ts
 })
 export class FoodComponent implements OnInit {
 
-	/**** tambahkan data nya disini. laper yang bakal kita panggil variabel
-	      untuk di food.component.html
-	*****/ 
-	laper: Foods = {
-		id: 1,
-		name: 'nasi uduk'
-	};
-	/**** 
-		end data
-	*****/ 
+	lapers = LAPERS; // ambil variabel LAPERS dari makanan-indo.ts
 
-	
+	// tambahkan yang dibawah ini ya
+	selectedLaper: Foods; // Foods diambil dari foods.ts
+
+	// laper diambil dari *ngFor food.component.html
+	onSelect(laper: Foods): void {
+		this.selectedLaper = laper;
+	}
+
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/makanan-indo.ts
@@ -0,0 +1,15 @@
+import { Foods } from './foods';
+
+export const LAPERS: Foods[] = [
+	{ id: 10, name: 'nasi uduk' },
+	{ id: 11, name: 'nasi goreng' },
+	{ id: 12, name: 'semur jengkol' },
+	{ id: 13, name: 'soto betawi' },
+	{ id: 14, name: 'sop buntung' },
+	{ id: 15, name: 'sop iga' },
+	{ id: 16, name: 'bakwan' },
+	{ id: 17, name: 'gudeg' },
+	{ id: 18, name: 'mie ayam' },
+	{ id: 19, name: 'mie goreng' },
+	{ id: 20, name: 'telor dadar' }
+];
